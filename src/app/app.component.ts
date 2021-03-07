a1f12e65623890ac87f957eaaee3cb00cd3203fa
Margkoss <markosg.stamatakis@gmail.com>
2021-03-07 21:13:01 +0200

    Add paymart title to main app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'pay-mart-frontend';
+export class AppComponent implements OnInit {
+  public title = 'PayMart | All in one payment solution for small transactions';
+
+  constructor(private readonly titleService: Title) {}
+
+  public ngOnInit(): void {
+    this.titleService.setTitle(this.title);
+  }
 }
