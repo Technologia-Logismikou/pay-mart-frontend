e83f1580940226d92a5e0abcc47047508a796f38
Margkoss <markosg.stamatakis@gmail.com>
2021-05-19 12:39:40 +0300

    Add login form and submit logic to login component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzMessageModule } from 'ng-zorro-antd/message';
 
+const ngZorroDeps = [NzFormModule, NzInputModule, NzButtonModule, NzMessageModule];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    declarations: [],
+    imports: [CommonModule, ...ngZorroDeps, ReactiveFormsModule, FormsModule],
+    exports: [...ngZorroDeps, ReactiveFormsModule, FormsModule],
 })
-export class SharedModule { }
+export class SharedModule {}
