df37c5a7eeeeef261e95228579f97f0dcad2f34e
Margkoss <markosg.stamatakis@gmail.com>
2021-05-20 19:04:27 +0300

    Add a landing page

--- src/app/login-register/login-register-routing.module.ts
+++ src/app/login-register/login-register-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: '', component: LandingPageComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
 ];
--- src/app/login-register/login-register.module.ts
+++ src/app/login-register/login-register.module.ts
@@ -5,9 +5,10 @@ import { LoginRegisterRoutingModule } from './login-register-routing.module';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { SharedModule } from '../shared/shared.module';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
-    declarations: [RegisterComponent, LoginComponent],
+    declarations: [RegisterComponent, LoginComponent, LandingPageComponent],
     imports: [CommonModule, LoginRegisterRoutingModule, SharedModule],
 })
 export class LoginRegisterModule {}
