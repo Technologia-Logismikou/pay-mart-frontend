e4113c76c22891fd330e51d50107f8083219ed6a
Margkoss <markosg.stamatakis@gmail.com>
2021-05-25 14:37:07 +0300

    Add tree table presentation of categories

--- src/app/dashboard/categories/categories.component.ts
+++ src/app/dashboard/categories/categories.component.ts
@@ -1,15 +1,77 @@
 import { Component, OnInit } from '@angular/core';
+import { categoriesToListOfMapData, convertTreeToList, TreeNodeInterface } from './category-tree-table-helpers';
 
 @Component({
-  selector: 'app-categories',
-  templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
+    selector: 'app-categories',
+    templateUrl: './categories.component.html',
+    styleUrls: ['./categories.component.scss'],
 })
 export class CategoriesComponent implements OnInit {
+    public categories = [
+        {
+            name: 'Κατωφόρια',
+            children: [
+                { name: 'Παντελόνι' },
+                {
+                    name: 'Shorts',
+                },
+            ],
+        },
+        {
+            name: 'Πανωφόρια',
+            children: [
+                {
+                    name: 'T-SHIRT',
+                    children: [
+                        {
+                            name: 'Απο μπάντες',
+                        },
+                        {
+                            name: 'Για τους υπόλοιπους τους φυσιλογικούς',
+                        },
+                    ],
+                },
+                {
+                    name: 'Πουκάμισα',
+                    children: [
+                        {
+                            name: 'Με ρίγες',
+                        },
+                    ],
+                },
+                {
+                    name: 'Πουκαολόκληρα',
+                },
+            ],
+        },
+    ];
+    constructor() {}
 
-  constructor() { }
+    listOfMapData: TreeNodeInterface[] = [];
+    mapOfExpandedData: { [key: string]: TreeNodeInterface[] } = {};
 
-  ngOnInit(): void {
-  }
+    public ngOnInit(): void {
+        this.listOfMapData = categoriesToListOfMapData(this.categories);
 
+        this.listOfMapData.forEach((item) => {
+            this.mapOfExpandedData[item.key] = convertTreeToList(item);
+        });
+
+        console.log(this.nestedCategoriesToCategoryList(this.categories));
+    }
+
+    private nestedCategoriesToCategoryList(categories: any[]): any[] {
+        let names: string[] = [];
+
+        for (let i = 0; i < categories.length; i++) {
+            names.push(categories[i].name);
+            if (categories[i].children) {
+                names.push(...this.nestedCategoriesToCategoryList(categories[i].children));
+            }
+        }
+
+        return names;
+    }
+
+    public createCategory(): void {}
 }
