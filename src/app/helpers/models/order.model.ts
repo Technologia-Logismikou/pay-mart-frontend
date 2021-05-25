7ada3a2bcc9a984a990094618b866dfc88ad2ace
Giotanna <giotanna9@gmail.com>
2021-05-25 17:16:52 +0300

    Add order model

---
+++ src/app/helpers/models/order.model.ts
@@ -0,0 +1,10 @@
+export interface Order {
+    customer: string;
+    card: string;
+    seller: string;
+    coupon: string;
+    products: string[];
+    county: string;
+    shippingAddress: string;
+    billingAddress: string;
+}
