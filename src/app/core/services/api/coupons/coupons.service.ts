02235d96fe22c6f887bec47872e4ed7ba5a1aa82
Giotanna <giotanna9@gmail.com>
2021-06-11 20:12:31 +0300

    Add Coupon CRUD service

---
+++ src/app/core/services/api/coupons/coupons.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Coupon, CouponDto } from 'src/app/helpers/models/coupon.model';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class CouponsService {
+    constructor(private readonly http: HttpClient) {}
+
+    public getCoupons(): Observable<Coupon[]> {
+        return this.http.get<Coupon[]>(
+            `${environment.backend.protocol}://${environment.backend.url}:${environment.backend.port}/coupons`
+        );
+    }
+
+    public createCoupon(coupon: CouponDto): Observable<Coupon> {
+        return this.http.post<Coupon>(
+            `${environment.backend.protocol}://${environment.backend.url}:${environment.backend.port}/coupons`,
+            coupon
+        );
+    }
+
+    public editCoupon(coupon: CouponDto, id: string): Observable<Coupon> {
+        return this.http.put<Coupon>(
+            `${environment.backend.protocol}://${environment.backend.url}:${environment.backend.port}/coupons/${id}`,
+            coupon
+        );
+    }
+
+    public deleteCoupon(id: string): Observable<Coupon> {
+        return this.http.delete<Coupon>(
+            `${environment.backend.protocol}://${environment.backend.url}:${environment.backend.port}/coupons`
+        );
+    }
+}
---
+++ src/app/helpers/models/coupon.model.ts
@@ -0,0 +1,24 @@
+export interface CouponDto {
+    name: string;
+    description: string;
+    amountOff: number;
+    percentOff: number;
+    timesRedeemed: number;
+    maxRedemptions: number;
+    expiration: string;
+    active: boolean;
+}
+
+export interface Coupon {
+    id: string;
+    createdAt: string;
+    updatedAt: string;
+    name: string;
+    description: string;
+    amountOff: number;
+    percentOff: number;
+    timesRedeemed: number;
+    maxRedemptions: number;
+    expiration: string;
+    active: boolean;
+}
